ed962976a8096f2dd4ee83316acf618e4e5d894e
Paul Carillion <pcarillion@yahoo.fr>
2020-02-26 15:35:46 +0100

    first chart created

--- src/components/simulateur/simResultsAreaChart.jsx
+++ src/components/simulateur/simResultsAreaChart.jsx
@@ -8,26 +8,55 @@ const simResultsAreaChart = (props) => {
     
 const data = [
   {
-    name: '2015', Transport: 4000, Logement: 2400, Industrie: 2400,
+        name: '2010', 
+        Energie: 12, 
+        Logement: 57, 
+        "Transport de personnes": 23, 
+        "Transport de marchandises": 88,
+        Industrie: 24,        
+        "Agriculture et Alimentation": 68,
+        "Consommation et service": 88
   },
   {
-    name: '2020', Transport: 3000, Logement: 1398, Industrie: 2210,
+        name: '2015', 
+        Energie: 13.24896964, 
+        Logement: 72.74804906, 
+        "Transport de personnes": 20.79017833, 
+        "Transport de marchandises": 102.0161185,
+        Industrie: 21.69409912,
+        "Agriculture et Alimentation": 61.46661418,
+        "Consommation et service": 102.0161185
   },
   {
-    name: '2025', Transport: 2000, Logement: 9800, Industrie: 2290,
+        name: '2020', 
+        Energie: 14.62793304, 
+        Logement: 92.84699373, 
+        "Transport de personnes": 18.79267456, 
+        "Transport de marchandises": 118.2646414,
+        Industrie: 19.60974737,
+        "Agriculture et Alimentation": 55.56095087,
+        "Consommation et service": 118.2646414
   },
   {
-    name: '2030', Transport: 2780, Logement: 3908, Industrie: 2000,
+        name: '2025', 
+        Energie: 16.15042006, 
+        Logement: 118.4989062, 
+        "Transport de personnes": 16.98708936, 
+        "Transport de marchandises": 137.1011327,
+        Industrie: 17.72565846,
+        "Agriculture et Alimentation": 50.22269898,
+        "Consommation et service": 137.1011327
   },
   {
-    name: '2040', Transport: 1890, Logement: 4800, Industrie: 2181,
-  },
-  {
-    name: '2050', Transport: 2390, Logement: 3800, Industrie: 2500,
-  },
-  {
-    name: '2040', Transport: 3490, Logement: 4300, Industrie: 2100,
-  },
+        name: '2030', 
+        Energie: 17.83136875, 
+        Logement: 151.2379692, 
+        "Transport de personnes": 15.35498335, 
+        "Transport de marchandises": 158.9377887,
+        Industrie: 16.02259132,
+        "Agriculture et Alimentation": 45.39734208,
+        "Consommation et service": 158.9377887
+  }
 ];
 
 
@@ -46,9 +75,13 @@ const data = [
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
-        <Area type="monotone" dataKey="Transport" stackId="1" stroke="#8884d8" fill="#8884d8" />
-        <Area type="monotone" dataKey="Logement" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-        <Area type="monotone" dataKey="Industrie" stackId="1" stroke="#ffc658" fill="#ffc658" />
+        <Area type="monotone" dataKey="Energie" stackId="1" stroke="#0047AB" fill="#0047AB" />
+        <Area type="monotone" dataKey="Logement" stackId="1" stroke="#4F86F7" fill="#4F86F7" />
+        <Area type="monotone" dataKey="Transport de personnes" stackId="1" stroke="#0067A5" fill="#0067A5" />
+        <Area type="monotone" dataKey="Transport de marchandises" stackId="1" stroke="#A1CAF1" fill="#A1CAF1" />
+        <Area type="monotone" dataKey="Industrie" stackId="1" stroke="#002366" fill="#002366" />
+        <Area type="monotone" dataKey="Agriculture et Alimentation" stackId="1" stroke="#b9bad3" fill=" #b9bad3" />
+        <Area type="monotone" dataKey="Consommation et Service" stackId="1" stroke="#326684" fill="#326684" />
         <Legend/>
       </AreaChart>
         </div>
