aa7bf7ffb057dc1e22dc3e474a4b928473f4b4a6
Paul Carillion <pcarillion@yahoo.fr>
2020-02-26 12:51:55 +0100

    sim list comp done

--- src/components/simulateur/simParametreList.jsx
+++ src/components/simulateur/simParametreList.jsx
@@ -4,7 +4,19 @@ const simParametreList = () => {
 
     return (
         <div>
-            
+            <h3>{props.parameters.data.name}</h3>
+            <p>{props.parameters.data.description}</p>
+            <form>
+            <div className="list" /*onChange=fonction callback*/>
+                {props.parameters.data.possibleValues.map((value, i) => {
+                    <>
+                        <input type="radio" id={`${value}`} name={`${value}`} value={`${value}`} checked={(props.parameters.data.value === value) ?  true : false }/>
+                        <label htmlFor={`${value}`}>{value}</label>
+                    </>
+                })
+                }
+			</div>
+            </form>
         </div>
     )
 }
