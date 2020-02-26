6c5630b0bfcfa0534cdf111e944924d470fe9b4d
Pascal Besson <pascalbes@gmail.com>
2020-02-26 13:47:06 +0100

    small fixes

--- src/components/simulateur/simParametreList.jsx
+++ src/components/simulateur/simParametreList.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 const simParametreList = (props) => {
 
+    console.log(props)
+
     const possibleValues = props.data.possibleValues.split(', ')
 
     return (
@@ -10,7 +12,7 @@ const simParametreList = (props) => {
             <p>{props.data.description}</p>
             {/* <form>
             <div className="list"> 
-                {props.data.possibleValues.map((value, i) => (
+                {possibleValues.map((value, i) => (
                     <>
                         <input type="radio" id={`${value}`} name={`${value}`} value={`${value}`} checked={(props.parameters.data.value === value) ?  true : false }/>
                         <label htmlFor={`${value}`}>{value}</label>
