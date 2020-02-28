b07441e20b69b3fab8ac747e1180f5ea0a10249c
Pascal Besson <pascalbes@gmail.com>
2020-02-28 21:40:10 +0100

    small fix

--- src/components/simulateur/simCategorie.jsx
+++ src/components/simulateur/simCategorie.jsx
@@ -6,7 +6,7 @@ const simCategorie = ({data, results}) => {
     console.log(results)
 
     return (
-        <div className="sim-categorie flex-item" id={"cat"+props.data.index}>
+        <div className="sim-categorie flex-item" id={"cat"+data.index}>
             <h3 className="sim-categorie-name">{data.name}</h3>
             {results && <Jauge results={results}/>}
             <p className="sim-categorie-description">{data.description}</p>
