a63560fb048d4d7da4838bbad09430f1392f053f
Nina Gautreau <nina.gautreau@gmail.com>
2020-03-01 18:43:21 +0100

    css results sim

--- src/components/resultats/parametres.jsx
+++ src/components/resultats/parametres.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import './../../styles/results-paramter.css'
+import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
+import { faFlag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const parametres = (props) => {
     return (
-        <div className="param-box">
-            <h3>{props.categorie.data.name}</h3>
-            <h4>{props.scope}</h4>
+        <div className="param-box light">
+            <div className="param-box-title flex-item">
+                {props.scope==="Émissions nationales" ? <FontAwesomeIcon className="right-btn" icon={faFlag}/> : <FontAwesomeIcon className="right-btn" icon={faGlobeAmericas}/>}
+                <h4>{props.categorie.data.name}</h4>
+            </div>
             {props.categorie.parameters.map((parameter, i) => {
-                return <p>{parameter.data.name}: {parameter.data.value} {parameter.data.unit}</p> 
+                return <p key={i}>{parameter.data.name} : {parameter.data.value} {parameter.data.unit}</p> 
             })}
         </div>
     )
