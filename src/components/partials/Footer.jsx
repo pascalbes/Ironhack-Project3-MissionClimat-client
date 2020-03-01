77fdce8c826f13a8024a2c89d35ee28a33d42efb
Nina Gautreau <nina.gautreau@gmail.com>
2020-03-01 17:21:44 +0100

    footer fix

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -4,10 +4,11 @@ const Footer = () => {
     return (
         <footer className="flex-item nomarge flex-item">
             <div className="footer-item flex-item">LOGO BOX</div>
-            <div className="footer-item flex-item flex-column">
-                <h3>Contactez-nous :</h3>
+            <div className="footer-item flex-item">
                 <a href="mailto:?subject=Mission-1.5%20:%20Contact%20particulier">Particuliers</a>
+                <div className="hidden">||</div>
                 <a href="mailto:?subject=Mission-1.5%20:%20Contact%20professionnel">Professionnels</a>
+                <div className="hidden">||</div>
                 <a href="mailto:?subject=Mission-1.5%20:%20J'ai%20repéré%20un%20bug">Signalez un bug</a>
             </div>
             <div className="footer-item flex-item">SOCIALS</div>
