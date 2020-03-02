ae650a93e8e0017a7a5fd70298cac9374162a904
Paul Carillion <pcarillion@yahoo.fr>
2020-03-02 16:26:55 +0100

    jauge responsive

--- src/components/simulateur/simJauge.jsx
+++ src/components/simulateur/simJauge.jsx
@@ -1,16 +1,20 @@
 
 import React from 'react'
-import { ResponsiveBullet } from '@nivo/bullet'
+import { Bullet } from '@nivo/bullet'
+import { AutoSizer } from 'react-virtualized'
 
 
 
 const MyBullet = ({results}) => {
 
     return (
-        <div>
-    <ResponsiveBullet
+        
+    <AutoSizer>
+        {({ height, width }) => (
+    <Bullet
         data={results}
-        // height={"10em"}
+        height={height}
+        width={width}
         // margin={{ top: 50, right: 90, bottom: 50, left: 90 }}
         spacing={46}
         measureColors="blue"
@@ -26,18 +30,21 @@ const MyBullet = ({results}) => {
         markerSize={1}
         reverse={true}
     />
-    {/* <ul style={{display: "flex", alignItems: "center", marginLeft: "10px"}}>
-        <li style={{display: "flex", alignItems: "center", marginLeft: "10px"}}>
-            <div style={{backgroundColor:"#D95F02", width: "5px", height: "13px", marginRight: "5px"}}></div>Business as usual
-        </li>
-        <li style={{display: "flex", alignItems: "center", marginLeft: "10px"}}>
-            <div style={{backgroundColor:"#1B9E77", width: "5px", height: "13px", marginRight: "5px"}}></div>Scénario 1.5°
-        </li>
-        <li style={{display: "flex", alignItems: "center", marginLeft: "10px"}}>
-            <div style={{backgroundColor:"blue", width: "13px", height: "13px", marginRight: "5px"}}></div>Votre paramètre
-        </li>
-    </ul> */}
-    </div>
+    )}
+</AutoSizer>
+  
+    //    <ul style={{display: "flex", alignItems: "center", marginLeft: "10px"}}>
+    //     <li style={{display: "flex", alignItems: "center", marginLeft: "10px"}}>
+    //         <div style={{backgroundColor:"#D95F02", width: "5px", height: "13px", marginRight: "5px"}}></div>Business as usual
+    //     </li>
+    //     <li style={{display: "flex", alignItems: "center", marginLeft: "10px"}}>
+    //         <div style={{backgroundColor:"#1B9E77", width: "5px", height: "13px", marginRight: "5px"}}></div>Scénario 1.5°
+    //     </li>
+    //     <li style={{display: "flex", alignItems: "center", marginLeft: "10px"}}>
+    //         <div style={{backgroundColor:"blue", width: "13px", height: "13px", marginRight: "5px"}}></div>Votre paramètre
+    //     </li>
+    // </ul>   
+   
     )
 }
 
