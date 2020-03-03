736a6c935f911d9a6a5657ba03fb38c6defb1efa
Paul Carillion <pcarillion@yahoo.fr>
2020-03-03 15:46:31 +0100

    forms and CRUD user finished

--- src/components/form/Signin.jsx
+++ src/components/form/Signin.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import { Redirect } from 'react-router-dom'
+import { withRouter } from "react-router";
 
-const Signin = () => {
-    function handleChange(){
-        "huhu"
-    }
+import UserContext from "../../auth/UserContext";
 
-    function handleSubmit(){
-        "huhu"
+
+import APIHandler from "../../api/APIHandler";
+
+
+const Signin = (props) => {
+
+    const [user, setUser] = useState({});
+    const userContext = useContext(UserContext);
+    const { setCurrentUser } = userContext;
+
+    
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const apiRes = await APIHandler.post("/session/signin", user);
+      console.log(apiRes)
+      setCurrentUser(apiRes.data.currentUser);
+      console.log(props)
+      props.history.push('/dashboard')
+    } catch (err) {
+      setCurrentUser(null);
     }
+  };
+
+  const handleChange = e => {
+    setUser({...user, [e.target.name]: e.target.value });
+  }
 
     return (
         <form className="form contact-form flex-item flex-column" onChange={handleChange} onSubmit={handleSubmit}>
             <label className="label" htmlFor="from">Votre adresse e-mail</label>
-            <input className="input border-btn" id="from" type="email" name="from" placeholder="example@domaine.com" required/>
+            <input className="input border-btn" id="from" type="email" name="email" placeholder="example@domaine.com" required/>
 
             <label className="label" htmlFor="password">Votre mot de passe</label>
             <input className="input border-btn" id="from" type="password" name="password" placeholder="••••••••" required/>
@@ -22,4 +45,4 @@ const Signin = () => {
     )
 }
 
-export default Signin
+export default withRouter(Signin)
--- src/components/form/Signup.jsx
+++ src/components/form/Signup.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, {useState, setState} from 'react'
+import APIHandler from "../../api/APIHandler";
+
 
 const Signup = () => {
-    function handleChange(){
-        "huhu"
-    }
 
-    function handleSubmit(){
-        "huhu"
-    }
+    const [user, setUser] = useState({});
+    // const [password, setPassword] = useState("password");
+    // const [newsletter, setNewsletter] = useState(true)
+
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        try {
+            await APIHandler.post("/session/signup", user);
+            console.log("ok");
+            
+            this.props.history.push("/session/signin");
+          } catch (err) {
+            console.error(err);
+          }
+      };
+
+      const handleChange = e => {
+          e.target.type !== "checkbox" ?
+        setUser({...user, [e.target.name]: e.target.value }) :
+        setUser({...user, [e.target.name]: e.target.checked});
+      };
+    
 
     return (
         <form className="form contact-form flex-item flex-column" onChange={handleChange} onSubmit={handleSubmit}>
-            <label className="label" htmlFor="username">Votre nom d'utilisateur</label>
-            <input className="input border-btn" id="username" type="string" name="username" placeholder="Pseudonyme" required/>
-            
+
             <label className="label" htmlFor="email">Votre adresse e-mail</label>
             <input className="input border-btn" id="email" type="email" name="email" placeholder="example@domaine.com" required/>
 
@@ -21,7 +38,7 @@ const Signup = () => {
             <input className="input border-btn" id="password" type="password" name="password" placeholder="••••••••" required/>
 
             <div className="news-box flex-item">
-                <input className="border-btn right-btn" type="checkbox" id="newsletter" name="newsletter" checked /> Je souhaite recevoir la newsletter
+                <input className="border-btn right-btn" type="checkbox" id="newsletter" name="isNewsLetter"  /> Je souhaite recevoir la newsletter
             </div>
 
             <button className="green-btn">Envoyer</button>
