bed3b67ad18c98a8dae60071288514e236186934
Nina Gautreau <nina.gautreau@gmail.com>
2020-03-06 09:42:15 +0100

    fix header

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import NavMain from "./NavMain"
 import { Link } from "react-router-dom"
 import "../../styles/header.css"
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const Header = () => {
+    const [title, setTitle] = useState("")
+
+    useEffect(() => {
+        window.location.pathname === "/" ? setTitle("") : setTitle("mission climat")
+    })
+
     return (
         <header id="scroll-top" className="flex-item">
             <div className="header-left flex-item">
                 <Link to="/"><img src="../../../images/logo/missionclimat.svg" alt="Home logo" className="header-logo"/></Link>
-                <h4>mission climat</h4>
+                <h4>{title}</h4>
             </div>
             <NavMain/>
         </header>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -15,9 +15,11 @@ const Home = () => {
                     <div className="hero-left">
                         <h1 className="border"><span className="hero-mission">mission</span><br/><span className="hero-temp">Climat</span></h1>
                         <p className="hero-text">Votre mission, si vous l'acceptez, est de proposer un scénario climat 2030 pour la France, permettant de limiter les impacts à un niveau soutenable. Pour se faire, nous mettons à votre disposition quelques paramètres, un modèle de calcul et un joli tableau de bord.</p>
-                        <p>Configurez, visualisez, et, une fois l'ensemble du vivant préservé, partagez : il paraît que c'est sympa !</p>
+                        <p className="hero-text hero-bold">Configurez, visualisez, et, une fois l'ensemble du <br/>vivant préservé, partagez : il paraît que c'est sympa !</p>
                         <br></br>
-                        <Link to="/simulator"><button className="green-btn">Mission acceptée !</button></Link>
+                        <div className="hero-btn flex-item">
+                            <Link to="/simulator"><button className="green-btn">Mission acceptée !</button></Link>
+                        </div>
                     </div>
                     <img className="hero-img" src="../../images/undraw_travel_plans_li01.svg" alt="landing-visual"/>
                 </div>
