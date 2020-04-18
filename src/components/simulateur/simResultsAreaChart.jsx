03c636c5545c791f933b75b403ae90bf92641983
Pascal Besson <pascalbes@gmail.com>
2020-04-18 10:57:50 +0200

    hover pie chart

--- src/components/simulateur/simResultsAreaChart.jsx
+++ src/components/simulateur/simResultsAreaChart.jsx
@@ -8,37 +8,6 @@ const simResultsAreaChart = ({datas}) => {
 
     const data = datas.data.data
 
-    console.log("areadatas")
-    console.log(data)
-    console.log(datas.areaDatas)
-
-    // function forTooltip(object) {
-    //   return Object.entries(object).map((array, i) =>{
-    //     return `${array[0]}: ${array[1]}`
-    //   })
-    // };
-  
-
-    // const CustomTooltip = (e) => {
-      // console.log(e);
-      
-    //   if (active) {
-    //     console.log(forTooltip(payload[0].payload))
-    //     return (
-    //       <div className="custom-tooltip">
-    //         {Object.entries(payload[0].payload).map((array, i) => {
-    //           return <p>{array[0]}: {array[1]} MGT CO2</p>
-    //         })}
-    //         {/* {forTooltip(payload[0].payload)} */}
-    //         {/* {`${payload.name} :${payload.value}`} */}
-    //         {/* <p className="intro">{getIntroOfPage(label)}</p> */}
-    //       </div>
-    //     );
-    // }
-    
-    //   return null;
-    // };
-
     function toolTipContent(e) {
       console.log(e)
       return (
@@ -50,21 +19,6 @@ const simResultsAreaChart = ({datas}) => {
         </div>
         )
     }
-
-
-//     payload: Array(5)
-// 0:
-// fillOpacity: "1"
-// stroke: "#0000CD"
-// fill: "#0000CD"
-// points: []
-// dataKey: "Bâtiments"
-// unit: undefined
-// formatter: undefined
-// name: "Bâtiments"
-// color: "#0000CD"
-// value: 111.6
-// type: undefined
 
     return (
       <ResponsiveContainer height= "100%" width="100%">
--- src/components/simulateur/sunburstChart.jsx
+++ src/components/simulateur/sunburstChart.jsx
@@ -5,22 +5,10 @@ import {PieChart, Pie, Sector, Cell, Legend, Label, Tooltip, ResponsiveContainer
   
 const SunburstChart = React.forwardRef(({datas}, ref) => {
 
-
-  // const RADIAN = Math.PI / 180;
-
-  // const renderCustomizedLabel = ({
-  //   cx, cy, midAngle, innerRadius, outerRadius, percent, index,
-  // }) => {
-  //    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  //   const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  //   const y = cy + radius * Math.sin(-midAngle * RADIAN);
-  
-  //   return (
-  //     <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-  //       {`${(percent * 100).toFixed(0)}%`}
-  //     </text>
-  //   );
-  // };
+  function toolTipContent(e) {
+    console.log(e)
+    return e.payload[0] ? <p style={{backgroundColor:'white',color:e.payload[0].payload.payload.color}}>{e.payload[0].name} : {Math.round(e.payload[0].value)} MtCO2</p> : ""
+  }
 
   return (
     <ResponsiveContainer width="100%" height="100%" >
@@ -34,7 +22,9 @@ const SunburstChart = React.forwardRef(({datas}, ref) => {
           {/* {datas.data02.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS2[index % COLORS2.length]} />)} */}
           <Label dataKey="value" position="outside"/>
         </Pie>
-        <Tooltip/>
+        <Tooltip
+          content={e => toolTipContent(e)}
+        />
       </PieChart>
     </ResponsiveContainer>
   )
