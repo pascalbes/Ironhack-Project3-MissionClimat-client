f498f2a36df4f9c9527f0aeae3e942621e77ef57
Pascal Besson <pascalbes@gmail.com>
2020-04-19 18:21:04 +0200

    header nav styled for simulator

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -7,13 +7,24 @@ import { useEffect } from 'react'
 import {withRouter} from 'react-router-dom'
 
 const Header = ({location}) => {
+    
     console.log(location)
+
+    function handleId() {
+        return location.pathname==="/simulator" ? "header-simulator" : ""
+    }
+
+    function handleColor() {
+        return location.pathname==="/simulator" ? "#163e59" : "white"
+    }
+
     return (
-        <header id="scroll-top" className="flex-item">
+        <header id={handleId()} className="flex-item">
             <div className="header-left flex-item">
-                <Link className="header-link flex-item" to="/"><img src="../../../images/logo/missionclimat.svg" alt="Home logo" className="header-logo"/>{location.pathname !=="/" && <h4>mission climat</h4>}</Link>
+                <Link className="header-link flex-item" to="/"><img src="../../../images/logo/missionclimat.svg" alt="Home logo" className="header-logo"/>
+                {location.pathname !=="/" && <h4>mission climat</h4>}</Link>
             </div>
-            <NavMain/>
+            <NavMain color={handleColor()}/>
         </header>
     )
 }
--- src/components/partials/NavMain.jsx
+++ src/components/partials/NavMain.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {useAuth} from "../../auth/useAuth"
-const NavMain = () => {
+const NavMain = (props) => {
     useAuth();
+    console.log(props)
     return (
         <nav>
-            <Link to="/simulator"><button className="border-btn left-btn">Simulateur</button></Link>
-            <Link to="/concept"><button className="border-btn left-btn">Concept</button></Link>
-            <Link to="/about"><button className="border-btn left-btn">L'équipe</button></Link>
-            <Link to="/contact"><button className="border-btn left-btn">Contact</button></Link>
-            <Link to="/dashboard"><button className="border-btn left-btn">Mon espace</button></Link>
+            <Link to="/simulator"><button className="border-btn left-btn" style={{color:props.color}}>Simulateur</button></Link>
+            <Link to="/concept"><button className="border-btn left-btn" style={{color:props.color}}>Concept</button></Link>
+            <Link to="/about"><button className="border-btn left-btn" style={{color:props.color}}>L'équipe</button></Link>
+            <Link to="/contact"><button className="border-btn left-btn" style={{color:props.color}}>Contact</button></Link>
+            <Link to="/dashboard"><button className="border-btn left-btn" style={{color:props.color}}>Mon espace</button></Link>
         </nav>
     )
 }
