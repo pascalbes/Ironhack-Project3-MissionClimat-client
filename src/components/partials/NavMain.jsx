9979a27c5ac260ad38db46389d74fa6d5130e9c1
Pascal Besson <pascalbes@gmail.com>
2020-04-21 13:24:40 +0200

    about page

--- src/components/partials/NavMain.jsx
+++ src/components/partials/NavMain.jsx
@@ -8,7 +8,7 @@ const NavMain = (props) => {
         <nav>
             <Link to="/simulator"><button className="border-btn left-btn" style={{color:props.color}}>Simulateur</button></Link>
             <Link to="/concept"><button className="border-btn left-btn" style={{color:props.color}}>Concept</button></Link>
-            <Link to="/about"><button className="border-btn left-btn" style={{color:props.color}}>L'équipe</button></Link>
+            <Link to="/about"><button className="border-btn left-btn" style={{color:props.color}}>À propos</button></Link>
             <Link to="/contact"><button className="border-btn left-btn" style={{color:props.color}}>Contact</button></Link>
             <Link to="/dashboard"><button className="border-btn left-btn" style={{color:props.color}}>Mon espace</button></Link>
         </nav>
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -15,43 +15,101 @@ export const AboutUs = () => {
             src="../../images/logo/Conception.svg"
             alt="Logo Ironhack"
           />
-          <h3 className="nomarge">Conception</h3>
+          <h3 className="nomarge">Historique du Projet</h3>
         </div>
+        <h5>L'étude 1.5 de BL évolution / Février 2019</h5>
         <p>
-          Mission Climat, c'est d'abord un travail d'équipe. Le projet a été
-          réalisé par une équipe de 5 personnes, toutes bénévoles, et a
-          bénéficié des éclairages de nombreux amis. En Octobre 2018, le Groupe
-          d’Experts Intergouvernemental sur l’Evolution du Climat (GIEC)
-          publiait un rapport spécial intitulé «Réchauffement planétaire de 1,5
-          °C» qui invitait l’ensemble des états à s’aligner sur une trajectoire
-          limitant le réchauffement climatique à 1,5°C pour éviter au maximum
-          ses conséquences dramatiques pour l’humanité.
+          L'histoire de ce projet débute avec la publication, en février 2019,
+          de l'étude <i><b>«Comment la France peut s’aligner sur une trajectoire compatible avec les
+          1,5°C»</b></i> par le cabinet de conseil BL évolution.
         </p>
         <p>
-          S’appuyant sur les données scientifiques contenues dans ce rapport,
-          cabinet de conseil B&L évolution a publié une étude intitulée «Comment
-          la France peut s’aligner sur une trajectoire compatible avec les
-          1,5°C». L’objectif de cette étude était de donner à voir les mesures
-          concrètes qui permettraient en ordre de grandeur de faire suffisamment
-          baisser les émissions de gaz à effet de serre de la France pour
-          s’aligner sur les recommandations des scientifiques du GIEC. En 2019,
-          la rencontre entre Guillaume, co-auteur de l’étude et Pascal, Paul et
-          Nina, jeunes développeurs engagés a donné vie à ce site internet
-          permettant à chaque citoyen·ne de construire son propre scénario
-          climat.
+          S’appuyant sur les données scientifiques contenues dans le rapport du <b>Groupe
+          d’Experts Intergouvernemental sur l’Evolution du Climat (GIEC)</b> intitulé
+          <i><b>«Réchauffement planétaire de 1,5°C»</b></i>, l’objectif de cette étude
+          était de donner à voir l'importance des mesures concrètes permettant de réduire
+          les émissions de gaz à effet de serre de la France pour s’aligner sur les recommandations
+          des scientifiques du GIEC, et limiter l'impact des conséquences du réchauffement climatique
+          à un niveau à peu près soutenable.
+        </p>
+        <p>
+          Liens étude et articles de presse
+        </p>
+        <h5>Développement v0 @Ironhack Paris / Février 2020</h5>
+        <p>
+          En janvier 2020, Guillaume Martin, l'un des 2 co-auteurs de l'étude, et Pascal Besson
+          décident de démocratiser et valoriser ce modèle autour d'un simulateur web, dont ils
+          ne savent pas encore qu'il s'appellera "Mission Transition". Une première maquette 
+          est alors développée en 2 semaines, dans le cadre du projet final
+          du bootcamp Ironhack Paris, auquel Pascal participe, avec Nina Gautreau et Paul Carillion.
+          En parallèle, Guillaume parfait le modèle avec l'aide de Célian Niclot.
+        </p>
+        <p>
+          Le projet, appelé tour à tour "Mon Scénario Climat", "Mission 1.5" puis "Mission Climat",
+          est finalement honoré en étant élu meilleur projets web lors des votes de la promotion
+          de dev web puis du hackshow du 9 mars. Champagne! Et repos...
+        </p>
+
+        <h5>Développement Mission Transition / Avril 2020</h5>
+        <p>
+          Après un cours repos, et la naissance de Flore Besson, une petite équipe est
+          montée pour finaliser le projet. Oriana Berthomieu définit l'UI finale du projet,
+          en s'inspirant du travail déjà réalisé par Nina, et Emmanuel Bernard rejoint Pascal
+          pour finaliser le développement.
+        </p>
+        <p>
+          Le site sort enfin le 3 mai 2020, au grand bonheur de tout ce joli monde, et nous l'espérons,
+          au vôtre également ! L'ensemble de l'équipe s'est investie bénévolement.
         </p>
       </article>
 
       <section className="about-lead-team flex-item">
         <article className="about-card about-card-lead border-btn  flex-item flex-column">
           <div className="about-card-head flex-item">
-            <img
+            {/* <img
               className="team-logo"
               src="../../images/logo/logoIH.svg"
               alt="Logo Ironhack"
-            />
-            <h3 className="nomarge">Développeurs</h3>
+            /> */}
+            <h3 className="nomarge">L'équipe</h3>
           </div>
+
+          <h5>Pilotage Projet</h5>
+          <p><a target="_blank" href="https://www.linkedin.com/in/pascal-besson/">
+                Pascal Besson
+              </a> et <a target="_blank" href="https://www.linkedin.com/in/guillaume-martin-86662989/">
+                    Guillaume Martin
+              </a>
+          </p>
+
+          <h5>Equipe développement</h5>
+          <p>Pascal Besson, <a target="_blank" href="https://www.linkedin.com/in/ninagautreau/">
+                    Nina Gautreau
+              </a>, <a target="_blank" href="https://www.linkedin.com/in/paul-carillion-3826041a1/">
+                    Paul Carillion
+              </a> et <a target="_blank" href="https://www.linkedin.com/in/emmbernard/">
+                    Emmanuel Bernard
+              </a>
+          </p>
+
+          <h5>Equipe données / modèle émissions</h5>
+          <p>Guillaume Martin, <a target="_blank" href="https://www.linkedin.com/in/charles-adrien-louis-63863526/">
+                    Charles-Adrien Louis
+              </a> et <a target="_blank" href="https://www.linkedin.com/in/c%C3%A9lian-niclot/">
+                    Célian Niclot
+              </a>
+          </p>
+
+          <h5>UX / UI</h5>
+          <p>Nina Gautreau et <a target="_blank" href="https://www.linkedin.com/in/oriana-berthomieu-3baa1431/">
+                    Oriana Berthomieu
+              </a>
+          </p>
+
+          <h5>Equipe tests</h5>
+          <p>
+                Estelle, Tom Nico et Pacco
+          </p>
 
           <p>
             Mission Climat a été développé en 2 semaines par 3 étudiants du
