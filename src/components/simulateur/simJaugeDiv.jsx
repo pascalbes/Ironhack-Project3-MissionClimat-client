fd13cb1903ec641e54bdc4dcbdf7cea0d9a5926e
Pascal Besson <pascalbes@gmail.com>
2020-05-01 09:28:47 +0200

    cursor finalized

--- src/components/simulateur/simJaugeDiv.jsx
+++ src/components/simulateur/simJaugeDiv.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 
 const simJaugeDiv = ({results}) => {
 
-    console.log(results)
-
-    const color=results[0].color
-
     const max = results[0].ranges[2];
     const m1 = (results[0].markers[0]/max)*100-0.25+"%";
     const m2 = (results[0].markers[1]/max-results[0].markers[0]/max)*100-0.25+"%";
@@ -41,19 +37,6 @@ const simJaugeDiv = ({results}) => {
    
     )
 
-    // const max = results[0].ranges[2];
-    // const value = results[0].measures[0]/max*100+"%";
-    // const m1 = (results[0].markers[0]/max-results[0].measures[0]/max)*100-0.25+"%";
-    // const m2 = (results[0].markers[1]/max-results[0].markers[0]/max)*100-0.25+"%";
-
-    // return (
-    //     <div className="jauge-ext" style={{height:'20px', width:'100%', backgroundColor:'white',border:'#C7C7C7 solid 1px', borderRadius:'10px'}}>
-    //         <div className="jauge-int" style={{height:'18.5px', width:`${value}`}}></div>
-    //         <div className="marker1" style={{height:'18.5px', width:'2px', position:'relative', marginLeft:`${m1}`, backgroundColor:'#0b8c85'}}></div>
-    //         <div className="marker2" style={{height:'18.5px', width:'2px', position:'relative', marginLeft:`${m2}`, backgroundColor:'#ff6868'}}></div>
-    //     </div>
-   
-    // )
 }
 
 export default simJaugeDiv
--- src/views/Simulator.jsx
+++ src/views/Simulator.jsx
@@ -435,7 +435,7 @@ const Simulator = (props) => {
             <div className="hidden">||</div>
             <h3 style={{color:'#7fffd4'}}>Initialisation</h3>
             <h4 className="light-text">Nous préparons votre environnement de travail.</h4>
-            <h4 className="light-text">L'attente ne devrait pas durer plus de 5 secondes</h4>
+            <h4 className="light-text">L'attente ne devrait pas durer plus de 5 secondes.</h4>
         </div>
  
     )
